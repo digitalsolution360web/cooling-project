1a62e369bcbb68ce21ed8cdae0ad908439e57b7b
Arun Verma <arunverma759954@gmail.com>
2026-05-04 21:23:15 +0530

    new update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -58,7 +58,7 @@ export default function Footer() {
 
                                 <li className="flex gap-4 group">
                                     <div className="text-accent shrink-0 mt-1"><svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg></div>
-                                    <p className="text-white/60 font-medium group-hover:text-white transition-colors text-sm">Shop No- 120, FF, Krishna Palace, <br />Gurugram 122001</p>
+                                    <p className="text-white/60 font-medium group-hover:text-white transition-colors text-sm">Unit No- 322, Precision Soho Tower Sector 67,<br />Badshahpur Gurgoan 122001</p>
                                 </li>
                                 <li className="flex gap-4 group">
                                     <div className="text-accent shrink-0 mt-1"><svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg></div>
